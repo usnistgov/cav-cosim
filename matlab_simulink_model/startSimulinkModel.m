a0d84731a0d962e1c5bce4068162fbb850e8991a
HadhoumH <hajjaj.hadhoum@gmail.com>
2025-05-08 13:40:41 -0400

    update simulink model to integrate ns3

--- matlab_simulink_model/startSimulinkModel.m
+++ matlab_simulink_model/startSimulinkModel.m
@@ -1,3 +1,34 @@
+% *
+%  * NIST-developed software is provided by NIST as a public service. You may use,
+%  * copy, and distribute copies of the software in any medium, provided that you
+%  * keep intact this entire notice. You may improve, modify, and create
+%  * derivative works of the software or any portion of the software, and you may
+%  * copy and distribute such modifications or works. Modified works should carry
+%  * a notice stating that you changed the software and should note the date and
+%  * nature of any such change. Please explicitly acknowledge the National
+%  * Institute of Standards and Technology as the source of the software. 
+%  *
+%  * NIST-developed software is expressly provided "AS IS." NIST MAKES NO WARRANTY
+%  * OF ANY KIND, EXPRESS, IMPLIED, IN FACT, OR ARISING BY OPERATION OF LAW,
+%  * INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTY OF MERCHANTABILITY,
+%  * FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND DATA ACCURACY. NIST
+%  * NEITHER REPRESENTS NOR WARRANTS THAT THE OPERATION OF THE SOFTWARE WILL BE
+%  * UNINTERRUPTED OR ERROR-FREE, OR THAT ANY DEFECTS WILL BE CORRECTED. NIST DOES
+%  * NOT WARRANT OR MAKE ANY REPRESENTATIONS REGARDING THE USE OF THE SOFTWARE OR
+%  * THE RESULTS THEREOF, INCLUDING BUT NOT LIMITED TO THE CORRECTNESS, ACCURACY,
+%  * RELIABILITY, OR USEFULNESS OF THE SOFTWARE.
+%  * 
+%  * You are solely responsible for determining the appropriateness of using and
+%  * distributing the software and you assume all risks associated with its use,
+%  * including but not limited to the risks and costs of program errors,
+%  * compliance with applicable laws, damage to or loss of data, programs or
+%  * equipment, and the unavailability or interruption of operation. This software 
+%  * is not intended to be used in any situation where a failure could cause risk
+%  * of injury or damage to property. The software developed by NIST employees is
+%  * not subject to copyright protection within the United States.
+%  *
+%  * Author: Hadhoum Hajjaj <hadhoum.hajjaj@nist.gov>
+% */
 clear; close all; clc;
 
 model_name = 'Traffic_Light_ROS2_demo';
@@ -10,7 +41,7 @@ load_system(model_name);
 set_param(model_name, 'SimulationCommand', 'update');  % Precompile for faster startup
 
 
-triggerTime = 10.0; % To select when to start running simulink model
+triggerTime = 1.0; % To select when to start running simulink model
 
 
 % Initialize ROS2 and Subscribe to /clock
